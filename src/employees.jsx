b986f929fc793ee3014e0ddf1dcfe56ee8854b31
Bob Painter <kuroppi@protonmail.com>
2023-05-22 00:56:08 -0700

    Initial Commit

---
+++ src/employees.jsx
@@ -0,0 +1,11 @@
+import EmployeeList from './EmployeeList.jsx'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <React.StrictMode>
+        <EmployeeList />
+    </React.StrictMode>,
+    document.getElementById('content')
+)
